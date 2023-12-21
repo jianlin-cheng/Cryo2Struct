c5e7cce63090afe1aa2e8230a57b9f003a252edd
Nabin Giri <39278928+nabingiri@users.noreply.github.com>
2023-12-20 20:45:22 -0600

    Create run_data_preparation.bash

---
+++ preprocess/run_data_preparation.bash
@@ -0,0 +1,12 @@
+# Execute data preparation script
+echo "Script started for data preparation"
+relative_path=$1
+density_map_dir=$(readlink -f "$relative_path")
+for i in "${density_map_dir[@]}"; do
+  echo "Running for density maps present in : $i"
+  python3 get_resample_map.py "$i"
+  python3 get_normalize_map.py "$i"
+  wait
+  echo "Done with maps in directory: $i"
+  echo "ALL DONE!" 
+done
