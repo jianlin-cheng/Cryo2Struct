f63386e66437c0d20dafea30ed6346cf375163bd
Nabin Giri <39278928+nabingiri@users.noreply.github.com>
2024-01-12 17:31:06 -0600

    Update run_data_preparation.bash
    Updated line 7 and 8 based on a raised issue.

--- preprocess/run_data_preparation.bash
+++ preprocess/run_data_preparation.bash
@@ -4,8 +4,8 @@ relative_path=$1
 density_map_dir=$(readlink -f "$relative_path")
 for i in "${density_map_dir[@]}"; do
   echo "Running for density maps present in : $i"
-  python3 get_resample_map.py "$i"
-  python3 get_normalize_map.py "$i"
+  python3 preprocess/get_resample_map.py "$i"
+  python3 preprocess/get_normalize_map.py "$i"
   wait
   echo "Done with maps in directory: $i"
   echo "ALL DONE!" 
